cdf7ccb636b77e8f34a8b2ff290d30707ac4102a
Surya <wirawansurya2311@gmail.com>
2025-08-03 17:15:17 +0700

    basic integral works with LSM, but some weird bumps with stationary axis

--- basic_integral.py
+++ basic_integral.py
@@ -1,7 +1,109 @@
 from data_utils import *
+import math
+import matplotlib.pyplot as plt
 
-sensor = read_sensor_file("raw_data/1x_move_yaw_pitch.csv")
-ori = orientation([0,0,0,0])
+# sensor = read_sensor_file("raw_data/1x_move_yaw_pitch.csv")
+sensor = read_sensor_file("raw_data/1x_roll.csv")
+# sensor = read_sensor_file("raw_data/2x_roll.csv")
+# sensor = read_sensor_file("raw_data/2x_yaw_pitch.csv")
+# sensor_still = read_sensor_file("raw_data/stationary_tilt.csv")
+sensor_still = read_sensor_file("raw_data/stationary.csv")
 
-for data in sensor:
-    print(data.lsm_accel_x)
+def cumulative_average(new_data, old_average, new_data_index):
+    n = new_data_index # n starts from 1
+    new_data = old_average * (n-1)/n + new_data / n
+
+    return new_data
+
+lsm_calib = xyz()
+for index,data in enumerate(sensor_still, start=1):
+    lsm_calib.x = cumulative_average(data.lsm.gyro.x, lsm_calib.x, index)
+    lsm_calib.y = cumulative_average(data.lsm.gyro.y, lsm_calib.y, index)
+    lsm_calib.z = cumulative_average(data.lsm.gyro.z, lsm_calib.z, index)
+
+for index,data in enumerate(sensor):
+    scale = 1
+    sensor[index].lsm.gyro.x -= lsm_calib.x * scale
+    sensor[index].lsm.gyro.y -= lsm_calib.y * scale
+    sensor[index].lsm.gyro.z -= lsm_calib.z * scale
+
+
+ori_lsm = []
+ori_mpu = []
+
+def rad_to_deg(ori: xyz):
+    ori_rad = xyz()
+    ori_rad.x = ori.x * 180.0 * (1/math.pi)
+    ori_rad.y = ori.y * 180.0 * (1/math.pi)
+    ori_rad.z = ori.z * 180.0 * (1/math.pi)
+
+    return ori_rad
+
+def deg_to_rad(ori: xyz):
+    ori_deg = xyz()
+    ori_deg.x = ori.x * math.pi * (1/180.0)
+    ori_deg.y = ori.y * math.pi * (1/180.0)
+    ori_deg.z = ori.z * math.pi * (1/180.0)
+
+    return ori_deg
+
+def accel_to_xy(accel: xyz):
+    ori = orientation()
+
+    ori.x = math.atan2(accel.x, accel.z) * 57.2958
+    val = math.sqrt(accel.x**2 + accel.z**2)
+    ori.y = math.atan2(accel.y, val) * 57.2958
+
+    return ori
+
+def gyro_local_to_global(gyro: xyz, ori_rad: xyz):
+    gy_g = xyz()
+
+    sin_x = math.sin(ori_rad.x)
+    sin_y = math.sin(ori_rad.y)
+    cos_x = math.cos(ori_rad.x)
+    cos_y = math.cos(ori_rad.y)
+
+    gy_g.x = gyro.x * cos_y + gyro.y * (sin_x * sin_y) + gyro.z * cos_x * sin_y
+    gy_g.y = gyro.y * cos_x + gyro.z * (-sin_x)
+    gy_g.z = gyro.x * (-sin_y) + gyro.y * (sin_x * cos_y) + gyro.z * cos_x * cos_y
+
+    return gy_g
+
+for index, data in enumerate(sensor):
+    # print(data.lsm.accel.x)
+    # break
+    time_dif = 0.01
+    last_z_lsm = 0
+    last_z_mpu = 0
+    if(index != 0):
+        last_z_lsm = ori_lsm[-1].z
+        last_z_mpu = ori_mpu[-1].z
+        
+    ori = accel_to_xy(data.lsm.accel)
+    ori.time = data.time
+
+    gyro = gyro_local_to_global(data.lsm.gyro, deg_to_rad(ori))
+    ori.z = last_z_lsm + gyro.z * time_dif
+    # print(ori.z)
+    ori_lsm.append(ori)
+
+    ori = accel_to_xy(data.mpu.accel)
+    ori.time = data.time
+    ori_mpu.append(ori)
+
+times = []
+x = []
+y = []
+z = []
+
+for ori in ori_lsm:
+    times.append(ori.time)
+    x.append(ori.x)
+    y.append(ori.y)
+    z.append(ori.z)
+
+plt.plot(times, x, color = "r")
+plt.plot(times, y, color = "g")
+plt.plot(times, z, color = "b")
+plt.show()
--- data_utils.py
+++ data_utils.py
@@ -1,39 +1,55 @@
 import csv
 import matplotlib.pyplot as plt
 
+class xyz():
+    def __init__(self):
+        self.time = 0
+        self.x = float(0)
+        self.y = float(0)
+        self.z = float(0)
+
 class sensor_data():
+    def __init__(self):
+        self.accel = xyz()
+        self.gyro = xyz()
+
+class sensor_file_data():
     def __init__(self, data):
         self.time = int(data[0])
-        self.lsm_accel_x = float(data[1]) * 0.000061
-        self.lsm_accel_y = float(data[2]) * 0.000061
-        self.lsm_accel_z = float(data[3]) * 0.000061
-        self.lsm_gyro_x = float(data[4]) * 0.00875
-        self.lsm_gyro_y = float(data[5]) * 0.00875
-        self.lsm_gyro_z = float(data[6]) * 0.00875
+
+        self.lsm = sensor_data()
+        self.mpu = sensor_data()
         
-        self.mpu_accel_x = float(data[7]) * 0.000061036
-        self.mpu_accel_y = float(data[8]) * 0.000061036
-        self.mpu_accel_z = float(data[9]) * 0.000061036
-        self.mpu_gyro_x = float(data[10]) * 0.0076295
-        self.mpu_gyro_y = float(data[11]) * 0.0076295
-        self.mpu_gyro_z = float(data[12]) * 0.0076295
+        self.lsm.accel.x = float(data[1]) * 0.000061
+        self.lsm.accel.y = float(data[2]) * 0.000061
+        self.lsm.accel.z = float(data[3]) * 0.000061
+        self.lsm.gyro.x = float(data[4]) * 0.00875
+        self.lsm.gyro.y = float(data[5]) * 0.00875
+        self.lsm.gyro.z = float(data[6]) * 0.00875
+        
+        self.mpu.accel.x = float(data[7]) * 0.000061036
+        self.mpu.accel.y = float(data[8]) * 0.000061036
+        self.mpu.accel.z = float(data[9]) * 0.000061036
+        self.mpu.gyro.x = float(data[10]) * 0.0076295
+        self.mpu.gyro.y = float(data[11]) * 0.0076295
+        self.mpu.gyro.z = float(data[12]) * 0.0076295
 
     # def __str__(self):
     #     return f"time:{self.time}ms, lsm accel({self.lsm_accel_x},{self.lsm_accel_y},{self.lsm_accel_z}),lsm gyro({self.lsm_gyro_x},{self.lsm_gyro_y},{self.lsm_gyro_z})"
 
 class orientation():
-    def __init__(self, data):
+    def __init__(self, data = [0,0,0,0]):
         self.time = int(data[0])
         self.x = float(data[1])
         self.y = float(data[2])
-        self.y = float(data[3])
+        self.z = float(data[3])
 
 def read_sensor_file(filename):
     datas = []
     with open(filename,'r') as fd:
         rd = csv.reader(fd)
         for row in rd:
-            datas.append(sensor_data(row))
+            datas.append(sensor_file_data(row))
             
     return datas
 
